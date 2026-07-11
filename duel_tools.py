e96fe0cbd53422ebc56d1fc3ae21ffa7064c8477
Pavel Petrov <pppetrv@gmail.com>
2026-07-11 12:02:09 +0300

    fix issues after duel command improvements

--- duel_tools.py
+++ duel_tools.py
@@ -9,5 +9,5 @@ DUEL_TTL = 86400  # Спор живёт 24 часа
 DUEL_PATTERN = re.compile(r"^/start duel_([a-f0-9]{8})$")
 
 
-def generate_duel_id()
+def generate_duel_id():
     return uuid.uuid4().hex[:8]
